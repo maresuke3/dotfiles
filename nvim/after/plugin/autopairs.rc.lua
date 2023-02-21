eaf04c66384d06e6a04bc3911821b7384a9f4d9e
daiki.fukushima <daiki.fukushima@geniee.co.jp>
2023-02-21 12:44:28 +0900

    fix: 不必要なファイルの削除と特定のファイルを適切な場所へ移動

--- nvim/after/plugin/autopairs.rc.lua
+++ nvim/after/plugin/autopairs.rc.lua
@@ -1,5 +1,11 @@
+print("load auto pairs")
 local status, autopairs = pcall(require, "nvim-autopairs")
-if (not status) then return end
+if (not status) then
+  print("not installed autopairs")
+  return
+end
+
+
 
 autopairs.setup({
   disable_filetype = { "TelescopePrompt" , "vim" },
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/maresuke/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/maresuke/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/maresuke/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/maresuke/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/maresuke/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/daiki-fukushima/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/daiki-fukushima/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/daiki-fukushima/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/daiki-fukushima/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/daiki-fukushima/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,122 +76,122 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-calc"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/cmp-calc",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/cmp-calc",
     url = "https://github.com/hrsh7th/cmp-calc"
   },
   ["cmp-emoji"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/cmp-emoji",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/cmp-emoji",
     url = "https://github.com/hrsh7th/cmp-emoji"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   ["cmp-vsnip"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
     url = "https://github.com/hrsh7th/cmp-vsnip"
   },
   ["lspkind.nvim"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
     url = "https://github.com/onsails/lspkind.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-autopairs"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope-file-browser.nvim"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/opt/telescope-file-browser.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/opt/telescope-file-browser.nvim",
     url = "https://github.com/nvim-telescope/telescope-file-browser.nvim"
   },
   ["telescope-ghq.nvim"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/opt/telescope-ghq.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/opt/telescope-ghq.nvim",
     url = "https://github.com/nvim-telescope/telescope-ghq.nvim"
   },
   ["telescope-z.nvim"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/opt/telescope-z.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/opt/telescope-z.nvim",
     url = "https://github.com/nvim-telescope/telescope-z.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim",
     wants = { "telescope-file-browser.nvim", "telescope-ghq.nvim", "telescope-z.nvim" }
   },
   ["vim-vsnip"] = {
     loaded = true,
-    path = "/Users/maresuke/.local/share/nvim/site/pack/packer/start/vim-vsnip",
+    path = "/Users/daiki-fukushima/.local/share/nvim/site/pack/packer/start/vim-vsnip",
     url = "https://github.com/hrsh7th/vim-vsnip"
   }
 }
