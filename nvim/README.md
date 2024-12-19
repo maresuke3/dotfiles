23d69199749643fffdc3a7f87b0e4ad4a9f4bf67
maresuke <fukusaku46@gmail.com>
2024-12-19 18:40:42 +0900

    clean up vim

--- nvim/README.md
+++
@@ -1,5 +0,0 @@
-# README
-
-## my neovim setting
-
-more detail is [here](https://halved-level-5b7.notion.site/Set-up-Neovim-984e82e0e7ce41699ef15953afdbc4cc)
--- nvim/after/plugin/ts-auto-pairs_config.lua
+++
@@ -1,7 +0,0 @@
-local status, autotag = pcall(require, 'nvim-ts-autotag')
-if (not status) then
-  print("not installed ts-autotag")
-  return
-end
-
-autotag.setup{}
